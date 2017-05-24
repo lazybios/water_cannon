19e657e395439d858a3a62d44de999c4b87e7668
lazybios <lazybios@gmail.com>
2017-05-24 15:58:55 +0800

    remove invalid chars

--- lib/water_cannon.rb
+++ lib/water_cannon.rb
@@ -2,7 +2,46 @@ require "water_cannon/version"
 
 module WaterCannon
   module String
+    QUOTE_CHARS = [
+      ["「", "‘"],
+      ["」", "’"],
+      ['『', '“'],
+      ['』', '”'],
+    ]
+
+    BULLET_CHARS = [
+      ['∙', '·'],
+      ['•', '·'],
+      ['・', '·'],
+      ['●', '·'],
+      # ['-', '·'], # only in name mode take effect
+      ['.', '·'],
+      ['．', '·']
+    ]
+
     def shoot!
+      # ' foo bar  ' => 'foo bar'
+      self.strip!
+
+      # 「」-『』=> '' - ""
+      QUOTE_CHARS.each do |chars|
+        self.gsub! chars[0], chars[1]
+      end
+
+      # 'foo ● bar' => 'foo · bar'
+      BULLET_CHARS.each do |chars|
+        self.gsub! chars[0], chars[1]
+      end
+
+      # remove space between bullet char
+      self.gsub! /\s+·\s+/, '·'
+
+      # '“ foo bar ”' => 'foo bar'
+      self.gsub! /^["'“‘](.*)[”’"']$/, '\1'
+
+      # replace multiple newlines with a single one:
+      self.gsub! /\n\n+/, "\n"
+
       self.strip
     end
   end
